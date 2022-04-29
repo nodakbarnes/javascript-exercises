cf81f85f74a11f004204dc3d46427d94d7351405
nodakbarnes <wally@nodakengineering.com>
2022-04-28 23:02:44 -0500

    Completed sumAll javascript exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    // return ERROR for non-number parameter
+    if (typeof start !== 'number' || typeof end !== 'number') return 'ERROR';
+    // return ERROR for negative number
+    if (start < 0 || end < 0) return 'ERROR';
+    // Swap if start larger than end
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+    let sum = 0;
+    // Loop through numbers and sum
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
